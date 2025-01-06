aef47642b58dfbad2abb1dd15c3e1c8e1ccc176a
Anton Kashnikov <anton.kashnikoff@icloud.com>
2025-01-06 16:46:18 +0100

    refactor loadLevel method

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -96,58 +96,73 @@ final class GameScene: SKScene {
         for (row, line) in lines.reversed().enumerated() {
             for (column, letter) in line.enumerated() {
                 let position = CGPoint(x: 64 * column + 32, y: 64 * row + 32)
-
-                switch letter {
-                case "x":
-                    // load wall
-                    let blockNode = SKSpriteNode(imageNamed: "block")
-                    blockNode.position = position
-                    blockNode.physicsBody = .init(rectangleOf: blockNode.size)
-                    blockNode.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
-                    blockNode.physicsBody?.isDynamic = false
-                    addChild(blockNode)
-                case "v":
-                    // load vortex
-                    let vortexNode = SKSpriteNode(imageNamed: "vortex")
-                    vortexNode.name = "vortex"
-                    vortexNode.position = position
-                    vortexNode.run(.repeatForever(.rotate(byAngle: .pi, duration: 1)))
-                    vortexNode.physicsBody = .init(circleOfRadius: vortexNode.size.width / 2)
-                    vortexNode.physicsBody?.isDynamic = false
-                    vortexNode.physicsBody?.categoryBitMask = CollisionTypes.vortex.rawValue
-                    vortexNode.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
-                    vortexNode.physicsBody?.collisionBitMask = .zero
-                    addChild(vortexNode)
-                case "s":
-                    // load star
-                    let starNode = SKSpriteNode(imageNamed: "star")
-                    starNode.name = "star"
-                    starNode.physicsBody = .init(circleOfRadius: starNode.size.width / 2)
-                    starNode.physicsBody?.isDynamic = false
-                    starNode.physicsBody?.categoryBitMask = CollisionTypes.star.rawValue
-                    starNode.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
-                    starNode.physicsBody?.collisionBitMask = .zero
-                    starNode.position = position
-                    addChild(starNode)
-                case "f":
-                    // load finish
-                    let finishNode = SKSpriteNode(imageNamed: "finish")
-                    finishNode.name = "finish"
-                    finishNode.physicsBody = .init(circleOfRadius: finishNode.size.width / 2)
-                    finishNode.physicsBody?.isDynamic = false
-                    finishNode.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
-                    finishNode.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
-                    finishNode.physicsBody?.collisionBitMask = .zero
-                    finishNode.position = position
-                    addChild(finishNode)
-                case " ":
-                    // this is an empty space – do nothing!
-                    break
-                default:
-                    fatalError("Unknown level letter: \(letter)")
-                }
+                addElement(with: letter, to: position)
             }
         }
+    }
+
+    private func addElement(with letter: Character, to position: CGPoint) {
+        switch letter {
+        case "x":
+            addWall(to: position)
+        case "v":
+            addVortex(to: position)
+        case "s":
+            addStar(to: position)
+        case "f":
+            addFinish(to: position)
+        case " ":
+            // this is an empty space – do nothing!
+            break
+        default:
+            fatalError("Unknown level letter: \(letter)")
+        }
+    }
+
+    private func addWall(to position: CGPoint) {
+        let blockNode = SKSpriteNode(imageNamed: "block")
+        blockNode.position = position
+        blockNode.physicsBody = .init(rectangleOf: blockNode.size)
+        blockNode.physicsBody?.categoryBitMask = CollisionTypes.wall.rawValue
+        blockNode.physicsBody?.isDynamic = false
+        addChild(blockNode)
+    }
+
+    private func addVortex(to position: CGPoint) {
+        let vortexNode = SKSpriteNode(imageNamed: "vortex")
+        vortexNode.name = "vortex"
+        vortexNode.position = position
+        vortexNode.run(.repeatForever(.rotate(byAngle: .pi, duration: 1)))
+        vortexNode.physicsBody = .init(circleOfRadius: vortexNode.size.width / 2)
+        vortexNode.physicsBody?.isDynamic = false
+        vortexNode.physicsBody?.categoryBitMask = CollisionTypes.vortex.rawValue
+        vortexNode.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        vortexNode.physicsBody?.collisionBitMask = .zero
+        addChild(vortexNode)
+    }
+
+    private func addStar(to position: CGPoint) {
+        let starNode = SKSpriteNode(imageNamed: "star")
+        starNode.name = "star"
+        starNode.physicsBody = .init(circleOfRadius: starNode.size.width / 2)
+        starNode.physicsBody?.isDynamic = false
+        starNode.physicsBody?.categoryBitMask = CollisionTypes.star.rawValue
+        starNode.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        starNode.physicsBody?.collisionBitMask = .zero
+        starNode.position = position
+        addChild(starNode)
+    }
+
+    private func addFinish(to position: CGPoint) {
+        let finishNode = SKSpriteNode(imageNamed: "finish")
+        finishNode.name = "finish"
+        finishNode.physicsBody = .init(circleOfRadius: finishNode.size.width / 2)
+        finishNode.physicsBody?.isDynamic = false
+        finishNode.physicsBody?.categoryBitMask = CollisionTypes.finish.rawValue
+        finishNode.physicsBody?.contactTestBitMask = CollisionTypes.player.rawValue
+        finishNode.physicsBody?.collisionBitMask = .zero
+        finishNode.position = position
+        addChild(finishNode)
     }
 
     private func createPlayer() {
